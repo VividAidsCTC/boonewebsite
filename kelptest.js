949586dc0e9d24bd59fb3fa8a6124277be448648
VividAidsCTC <57645224+VividAidsCTC@users.noreply.github.com>
2025-06-11 16:09:43 -0700

    Update kelptest.js

--- kelptest.js
+++ kelptest.js
@@ -432,7 +432,7 @@ function loadGLTFKelp() {
             template.position.y = -1; // Place on seafloor level
 
             // Create 500 kelp instances
-            for(let i = 0; i < 400; i++) {
+            for(let i = 0; i < 200; i++) {
                 const kelpInstance = template.clone();
 
                 // Position kelp on the seafloor in tighter formation
