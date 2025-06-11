6fed857ea1d0614017025c3a746639125de1361e
VividAidsCTC <57645224+VividAidsCTC@users.noreply.github.com>
2025-06-11 14:19:10 -0700

    Update fog.js

--- fog.js
+++ fog.js
@@ -17,7 +17,7 @@ const FOG_CONFIG = {
     // Default underwater fog settings
     color: 0x001133,        // Dark blue to match background gradient
     near: 30,               // Distance where fog starts (units)
-    far: 450,               // Distance where fog is maximum (units)
+    far: 300,               // Distance where fog is maximum (units)
     intensity: 1.0,         // Fog intensity multiplier
     enabled: true           // Whether fog is active
 };
