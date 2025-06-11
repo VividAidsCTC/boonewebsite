818fa852a160dfd53061450e67175d86c1b1305d
VividAidsCTC <57645224+VividAidsCTC@users.noreply.github.com>
2025-06-11 15:31:18 -0700

    Update kelptest.js

--- kelptest.js
+++ kelptest.js
@@ -804,7 +804,7 @@ function animate() {
     });
 
         if (typeof OceanParticles !== 'undefined') {
-        OceanParticles.update(.016 * waveSpeed); // Use your existing deltaTime
+        OceanParticles.update(.01 * waveSpeed); // Use your existing deltaTime
     }
 
 
