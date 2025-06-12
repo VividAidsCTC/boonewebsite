dbe9c6d600c10cb4ff9d67f76d00d24345248cbb
VividAidsCTC <57645224+VividAidsCTC@users.noreply.github.com>
2025-06-12 15:07:40 -0700

    Update surface.js

--- surface.js
+++ surface.js
@@ -6,8 +6,8 @@ let oscillatingTime = 0;
 const PLANE_CONFIG = {
     width: 2000,
     height: 2000,
-    segments: 256,
-    yPosition: 70,
+    segments: 512,
+    yPosition: 50,
     amplitude: 1.0,        // Reduced for smaller waves
     frequency: 0.05,       // Increased for more wave parts
     speed: 1.0,
