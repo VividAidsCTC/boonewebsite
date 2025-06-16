0c649975267c1918f87ae5da3415e4e6861a8694
VividAidsCTC <57645224+VividAidsCTC@users.noreply.github.com>
2025-06-16 14:08:15 -0700

    Update kelptest.js

--- kelptest.js
+++ kelptest.js
@@ -15,7 +15,7 @@ let isMouseDown = false;
 let floorTextures = { diffuse: null, normal: null, roughness: null, displacement: null };
 let textureLoader = new THREE.TextureLoader();
 
-const KELP_COUNT = 300;
+const KELP_COUNT = 250;
 let instanceData = [];
 let kelpGeometry = null;
 let kelpMaterial = null;
